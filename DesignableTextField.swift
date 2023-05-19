7c403d4840fc3aea1f6220f21dd2c489527f2b82
Hitesh Takhtani <hitesht.itpath@gmail.com>
2023-05-19 10:45:49 +0530

    Added Customized UItextfield

---
+++ DesignableTextField.swift
@@ -0,0 +1,49 @@
+//Use this class to UITextField to remove all borders from text field except bottom, also reduce the space betwen text in textfield and bottomBorderLine with the help of bottomLineY
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class DesignableTextField: UITextField {
+    
+    @IBInspectable var bottomLineColor: UIColor = UIColor.black {
+        didSet {
+            updateBottomLine()
+        }
+    }
+    
+    @IBInspectable var bottomLineHeight: CGFloat = 2.0 {
+        didSet {
+            updateBottomLine()
+        }
+    }
+    
+    @IBInspectable var bottomLineY: CGFloat = 5.0 {
+        didSet {
+            updateBottomLine()
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        addBottomLine()
+    }
+    
+    private func addBottomLine() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.height - bottomLineY, width: self.frame.width, height: bottomLineHeight)
+        bottomLine.backgroundColor = bottomLineColor.cgColor
+        self.borderStyle = .none
+        self.layer.addSublayer(bottomLine)
+    }
+    
+    private func updateBottomLine() {
+        if let sublayers = self.layer.sublayers {
+            for layer in sublayers {
+                if layer.frame.height == bottomLineHeight {
+                    layer.backgroundColor = bottomLineColor.cgColor
+                }
+            }
+        }
+    }
+}
